9707c4b82628e5e304ddd028354273ca62b1b011
Jasmine Foong <jasfoong19@gmail.com>
2024-05-14 23:10:22 -0700

    add 2 routes for homepage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.scss';
+import Header from '../components/Header/Header'
+import Characters from '../components/Characters/Characters'
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <Characters />
+    </>
+  );
+}
+
+export default HomePage;
