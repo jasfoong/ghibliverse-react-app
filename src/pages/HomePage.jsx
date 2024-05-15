bd554d3903bc3bacd21c243dad59fa8aceb044dd
Jasmine Foong <jasfoong19@gmail.com>
2024-05-14 23:13:00 -0700

    add CharactersApi class with 2 get request methods

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import './HomePage.scss';
 import Header from '../components/Header/Header'
 import Characters from '../components/Characters/Characters'
+import CharactersApi from '../classes/characters-api'
 
 function HomePage() {
+
+// const videosApi = new VideosApi(serverURL);
   return (
     <>
       <Header />
