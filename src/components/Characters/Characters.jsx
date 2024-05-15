e5d5fbe023460724fdbf9c07f9d732c5f5a97573
Jasmine Foong <jasfoong19@gmail.com>
2024-05-15 06:45:51 -0700

    add character properties based on dataset updates

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,18 +1,22 @@
 import './Characters.scss'
 import haku from '../../assets/images/haku.jpeg'
+import { Link } from 'react-router-dom'
 
 const CharacterList = () => {
     return (
         <main className="characters">
-            <div>
+            {/* <Link className="characters__link" to={`/characters/${character.id}`} key={character.id}> */}
                 <div className="characters__card">
                     <img className="characters__image" src={haku} alt="character"/>
                     <h3 className="characters__name">name</h3>
                     <h5 className="characters__age">age</h5>
-                    <h5 className="characters__short-description">short description</h5>
-                    <p className="characters__long-description">long description</p>
+                    <h5 className="characters__species">species</h5>
+                    <h5 className="characters__movie">movie</h5>
+                    <h5 className="characters__powers">special powers</h5>
+                    <h5 className="characters__description">description</h5>
+                    <p className="characters__extended-description">extended-description</p>
                 </div>
-            </div>
+            {/* </Link> */}
         </main>
     )
 }
